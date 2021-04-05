- fixed seed workers dataloader - enabled mixed precision

architectures/memory.py
@@ -98,6 +98,8 @@ class EncoderMemoryWrap(nn.Module):
         # layers
         self.encoder = encoder
         self.fc = MLP(final_input_dim,final_input_dim*2,output_dim)
+
+
     def _vector_norms(self, v):
         """ Computes the vector norms
         Args:
@@ -143,6 +145,68 @@ class EncoderMemoryWrap(nn.Module):
             return output
 
 
+class TLMemoryWrap(nn.Module):
+
+    def __init__(self, encoder, encoder_output_dim, output_dim, freeze_encoder=True):
+        super(TLMemoryWrap, self).__init__()
+
+        self.in_features = encoder_output_dim
+        self.out_features = output_dim
+        self.out_memory_dim = encoder_output_dim
+        final_input_dim = encoder_output_dim
+        
+        # layers
+        self.encoder = encoder
+
+        if freeze_encoder:
+            for param in self.encoder.parameters():
+                param.requires_grad = False
+
+        self.fc = MLP(final_input_dim,final_input_dim*2,output_dim)
+
+    def _vector_norms(self, v):
+        """ Computes the vector norms
+        Args:
+            v: The vector from which there must be calculated the norms
+
+        Returns:
+             A tensor containing the norms of input vector v
+        """
+
+        squared_norms = torch.sum(v * v, dim=1, keepdim=True)
+        return torch.sqrt(squared_norms + _EPSILON)
+        
+    def forward(self, inputs, supportset, return_weights=False):
+        """Forward call of MemoryWrap.
+        Args:
+            input: A tensor of dimensions [b,dim] where dim is the dimension required by the encoder
+            supportset: Support set. A tensor of dimension [m,dim] where m is the number of examples in memory
+            parsed_memory: a flag to indicate if the support set is already parsed by the encoder
+        Returns:
+            A tuple `(output, content-weight)` where `output`
+            is the output tensor, `content_weights` is a tensor containing the
+            read weights for sample in memory. If return_weights is False, then
+            only `output` is returned.
+        """
+      
+
+        encoder_output =  self.encoder(inputs)
+        memory = self.encoder(supportset)
+
+        encoder_norm = encoder_output / self._vector_norms(encoder_output)
+        memory_norm = memory / self._vector_norms(memory)
+        sim = torch.mm(encoder_norm,memory_norm.transpose(0,1))
+        content_weights = sparsemax(sim,dim=1)
+        support_vector = torch.matmul(content_weights,memory)
+        output = self.fc(support_vector)
+
+        if return_weights:
+            return output, content_weights
+        else: 
+            return output
+
+
+
 class MemoryWrapLayer(nn.Module):
 
     def __init__(self, encoder_output_dim, output_dim):
@@ -168,7 +232,7 @@ class MemoryWrapLayer(nn.Module):
         squared_norms = torch.sum(v * v, dim=1, keepdim=True)
         return torch.sqrt(squared_norms + _EPSILON)
         
-    def forward(self, encoder_output, memory,parsed_memory=False, return_weights=False):
+    def forward(self, encoder_output, memory, return_weights=False):
         """Forward call of MemoryWrap.
         Args:
             input: A tensor of dimensions [b,dim] where dim is the dimension required by the encoder
@@ -217,7 +281,7 @@ class EncoderMemoryWrapLayer(nn.Module):
         squared_norms = torch.sum(v * v, dim=1, keepdim=True)
         return torch.sqrt(squared_norms + _EPSILON)
         
-    def forward(self, encoder_output, memory,parsed_memory=False, return_weights=False):
+    def forward(self, encoder_output, memory, return_weights=False):
         """Forward call of MemoryWrap.
         Args:
             input: A tensor of dimensions [b,dim] where dim is the dimension required by the encoder

aux.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 from architectures import resnet
 from architectures.mobilenet import MobileNetV2
 from architectures.efficientnet import EfficientNetB0
-from architectures.memory import MemoryWrap,EncoderMemoryWrap
-from architectures.memory import Identity
+# from architectures.memory import MemoryWrap,EncoderMemoryWrap
+# from architectures.memory import Identity
 import statistics
 from architectures.memory_efficientnet import MemoryEfficientNetB0, EncoderMemoryEfficientNetB0
 from architectures.memory_mobilenet import MemoryMobileNetV2,EncoderMemoryMobileNetV2
@@ -25,6 +25,7 @@ def get_model(model_name, num_classes, model_type='memory'):
             model = EncoderMemoryResNet18()
         else:
             model = resnet.ResNet18()
+
     elif model_name == 'mobilenet':
         if model_type=='memory':
             model = MemoryMobileNetV2()

datasets.py
@@ -1,6 +1,8 @@
 
 import torch
 import torchvision
+import numpy
+import random
 
 def seed_worker(worker_id):
     worker_seed = torch.initial_seed() % 2**32
@@ -58,18 +60,18 @@ def get_SVHN(data_dir, batch_size_train, batch_size_test,batch_size_memory,size_
             normalize,
     ])
     
-    train_data = torchvision.datasets.SVHN(data_dir, split='train', download=False, transform=transforms)
-    test_data =  torchvision.datasets.SVHN(data_dir, split='test', download=False, transform=transforms)
+    train_data = torchvision.datasets.SVHN(data_dir, split='train', download=True, transform=transforms)
+    test_data =  torchvision.datasets.SVHN(data_dir, split='test', download=True, transform=transforms)
     train_dataset, val_dataset = split_dataset(train_data,size_train,6000,seed)
 
 
-    train_loader = torch.utils.data.DataLoader( train_dataset, batch_size=batch_size_train,pin_memory=True, shuffle=True, drop_last=True,worker_init_fn=seed)
+    train_loader = torch.utils.data.DataLoader( train_dataset, batch_size=batch_size_train,num_workers=2,pin_memory=True, shuffle=True, drop_last=True,worker_init_fn=seed_worker)
 
-    val_loader = torch.utils.data.DataLoader( val_dataset, batch_size=batch_size_test,pin_memory=True, shuffle=False,worker_init_fn=seed)
+    val_loader = torch.utils.data.DataLoader( val_dataset, batch_size=batch_size_test,num_workers=2,pin_memory=True, shuffle=False,worker_init_fn=seed_worker)
 
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size_test,pin_memory=True, shuffle=False,worker_init_fn=seed)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size_test,num_workers=2,pin_memory=True, shuffle=False,worker_init_fn=seed_worker)
 
-    mem_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size_memory,pin_memory=True, shuffle=True, drop_last=True,worker_init_fn=seed)
+    mem_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size_memory,num_workers=2,pin_memory=True, shuffle=True, drop_last=True,worker_init_fn=seed_worker)
     
     return train_loader, val_loader, test_loader, mem_loader
 
@@ -88,20 +90,20 @@ def get_CIFAR10(data_dir, batch_size_train, batch_size_test,batch_size_memory,si
                normalize
     ])
 
-    train_data = torchvision.datasets.CIFAR10(data_dir, train=True, download=False, transform=transforms_train)
-    test_data = torchvision.datasets.CIFAR10(data_dir, train=False, download=False, transform=transforms_test)
+    train_data = torchvision.datasets.CIFAR10(data_dir, train=True, download=True, transform=transforms_train)
+    test_data = torchvision.datasets.CIFAR10(data_dir, train=False, download=True, transform=transforms_test)
     
 
     train_dataset, val_dataset = split_dataset(train_data,size_train,6000,seed)
 
 
-    train_loader = torch.utils.data.DataLoader( train_dataset,num_workers=4, batch_size=batch_size_train,pin_memory=False, shuffle=True, drop_last=True)
+    train_loader = torch.utils.data.DataLoader( train_dataset,num_workers=2, batch_size=batch_size_train,pin_memory=False, shuffle=True, drop_last=True,worker_init_fn=seed_worker)
 
-    val_loader = torch.utils.data.DataLoader( val_dataset, batch_size=batch_size_test,pin_memory=False)
+    val_loader = torch.utils.data.DataLoader( val_dataset, batch_size=batch_size_test,num_workers=2,pin_memory=False,worker_init_fn=seed_worker)
 
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size_test,pin_memory=False)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size_test,num_workers=2,pin_memory=False,worker_init_fn=seed_worker)
 
-    mem_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size_memory,pin_memory=False, shuffle=True, drop_last=True)
+    mem_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size_memory,num_workers=2,pin_memory=False, shuffle=True, drop_last=True,worker_init_fn=seed_worker)
     
     return train_loader, val_loader, test_loader, mem_loader
 
@@ -118,19 +120,19 @@ def get_CIFAR100(data_dir, batch_size_train, batch_size_test,batch_size_memory,s
                normalize
     ])
 
-    train_data = torchvision.datasets.CIFAR100(data_dir, train=True, download=False, transform=transforms_train)
-    test_data = torchvision.datasets.CIFAR100(data_dir, train=False, download=False, transform=transforms_test)
+    train_data = torchvision.datasets.CIFAR100(data_dir, train=True, download=True, transform=transforms_train)
+    test_data = torchvision.datasets.CIFAR100(data_dir, train=False, download=True, transform=transforms_test)
     
 
     train_dataset, val_dataset = split_dataset(train_data,size_train,6000,seed)
 
 
-    train_loader = torch.utils.data.DataLoader( train_dataset,num_workers=4, batch_size=batch_size_train,pin_memory=False, shuffle=True, drop_last=True)
+    train_loader = torch.utils.data.DataLoader( train_dataset,num_workers=2, batch_size=batch_size_train,pin_memory=False, shuffle=True, drop_last=True,worker_init_fn=seed_worker)
 
-    val_loader = torch.utils.data.DataLoader( val_dataset, batch_size=batch_size_test,pin_memory=False)
+    val_loader = torch.utils.data.DataLoader( val_dataset, batch_size=batch_size_test,num_workers=2,pin_memory=False,worker_init_fn=seed_worker)
 
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size_test,pin_memory=False)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size_test,num_workers=2,pin_memory=False,worker_init_fn=seed_worker)
 
-    mem_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size_memory,pin_memory=False, shuffle=True, drop_last=True)
+    mem_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size_memory,num_workers=2,pin_memory=False, shuffle=True, drop_last=True,worker_init_fn=seed_worker)
     
     return train_loader, val_loader, test_loader, mem_loader

train.py
@@ -30,16 +30,16 @@ def set_seed(seed):
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
     return random_state
 
-def train_step(model, inputs, targets, optimizer, loss_criterion):    
-    optimizer.zero_grad() # zeroing gradients
-    if type(inputs) is tuple:
-        outputs  = model(*inputs) # get output
-    else:
-        outputs = model(inputs)
-    loss = loss_criterion(outputs, targets)  # compute loss
-    loss.backward() # compute gradients
-    optimizer.step() # update weights
-    return loss
+# def train_step(model, inputs, targets, optimizer, loss_criterion):    
+#     optimizer.zero_grad() # zeroing gradients
+#     if type(inputs) is tuple:
+#         outputs  = model(*inputs) # get output
+#     else:
+#         outputs = model(inputs)
+#     loss = loss_criterion(outputs, targets)  # compute loss
+#     loss.backward() # compute gradients
+#     optimizer.step() # update weights
+#     return loss
 
 def train_memory_model(model,loaders,optimizer,scheduler, loss_criterion, num_epochs,device):
         
@@ -48,8 +48,11 @@ def train_memory_model(model,loaders,optimizer,scheduler, loss_criterion, num_ep
     # training process 
     model.train()
 
+    scaler = torch.cuda.amp.GradScaler()
     for epoch in range(1, num_epochs + 1):
         for batch_idx, (data, y) in enumerate(train_loader):
+            
+            optimizer.zero_grad()
             # input
             data = data.to(device)
             y = y.to(device)
@@ -57,7 +60,14 @@ def train_memory_model(model,loaders,optimizer,scheduler, loss_criterion, num_ep
             memory_input = memory_input.to(device)
             
             # perform training step
-            train_step(model=model,inputs=(data,memory_input),targets=y,optimizer=optimizer,loss_criterion=loss_criterion)
+            with torch.cuda.amp.autocast():
+                outputs  = model(data,memory_input)
+                loss = loss_criterion(outputs, y)
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+            #train_step(model=model,inputs=(data,memory_input),targets=y,optimizer=optimizer,loss_criterion=loss_criterion)
 
             #log stuff
             if batch_idx % FLAGS.log_interval == 0:
@@ -75,15 +85,23 @@ def train_std_model(model,train_loader,optimizer,scheduler, loss_criterion, num_
         
     # training process
     model.train()  
+    scaler = torch.cuda.amp.GradScaler()
     for epoch in range(1, num_epochs + 1):      
-        for batch_idx, (data, y) in enumerate(train_loader):  
+        for batch_idx, (data, y) in enumerate(train_loader): 
+            optimizer.zero_grad() 
             # input
             data = data.to(device)
             y = y.to(device)
             
             # training step
-            train_step(model=model,inputs=data,targets=y,optimizer=optimizer,loss_criterion=loss_criterion)
+            #train_step(model=model,inputs=data,targets=y,optimizer=optimizer,loss_criterion=loss_criterion)
+            with torch.cuda.amp.autocast():
+                outputs  = model(data)
+                loss = loss_criterion(outputs, y)
 
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
             # log stuff
             if batch_idx % FLAGS.log_interval == 0:
                 print('Train Epoch: {} [({:.0f}%({})]\t'.format(