- fixed zeroing gradient

only_train.py
@@ -50,7 +50,7 @@ def train_memory_model(model,loaders,optimizer,scheduler, loss_criterion, num_ep
     scaler = torch.cuda.amp.GradScaler()
     for epoch in range(1, num_epochs + 1):
         for batch_idx, (data, y) in enumerate(train_loader):
-            
+            optimizer.zero_grad()
             # input
             data = data.to(device)
             y = y.to(device)
@@ -58,8 +58,14 @@ def train_memory_model(model,loaders,optimizer,scheduler, loss_criterion, num_ep
             memory_input = memory_input.to(device)
             
             # perform training step
-            train_step(model=model,inputs=(data,memory_input),targets=y,optimizer=optimizer,loss_criterion=loss_criterion)
+            #train_step(model=model,inputs=(data,memory_input),targets=y,optimizer=optimizer,loss_criterion=loss_criterion)
+            with torch.cuda.amp.autocast():
+                    outputs  = model(data,memory_input)
+                    loss = loss_criterion(outputs, y)
 
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
 
 
         scheduler.step()# increase scheduler step for each epoch
@@ -75,13 +81,20 @@ def train_std_model(model,train_loader,optimizer,scheduler, loss_criterion, num_
     scaler = torch.cuda.amp.GradScaler()
     for epoch in range(1, num_epochs + 1):      
         for batch_idx, (data, y) in enumerate(train_loader): 
+            optimizer.zero_grad()
             # input
             data = data.to(device)
             y = y.to(device)
             
             # training step
-            train_step(model=model,inputs=data,targets=y,optimizer=optimizer,loss_criterion=loss_criterion)
+            t#rain_step(model=model,inputs=data,targets=y,optimizer=optimizer,loss_criterion=loss_criterion)
+            with torch.cuda.amp.autocast():
+                outputs  = model(data)
+                loss = loss_criterion(outputs, y)
 
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
         
         scheduler.step() # increase scheduler step for each epoch
 