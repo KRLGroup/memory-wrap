eba057b0fda9da12dc05a45c8852b3d893f389de
biagiomattialarosa <biagiomattialarosa@gmail.com>
2021-06-02 12:09:43 +0200

    uniformed eval.py to other scripts format

--- eval.py
+++ eval.py
@@ -2,34 +2,35 @@ import absl.flags
 import absl.app
 import torch # type: ignore
 import numpy as np
-import random
 import datasets
 import aux
 
 # user flags
 absl.flags.DEFINE_string("path_model", None, "Path of the trained model")
-absl.flags.DEFINE_string("dataset", None, "Dataset to test (SVHN or CIFAR10)")
-absl.flags.DEFINE_string("modality", None, "Fixed or random")
 
-absl.flags.mark_flag_as_required("dataset")
 absl.flags.mark_flag_as_required("path_model")
-absl.flags.mark_flag_as_required("modality")
 
 FLAGS = absl.flags.FLAGS
 
 
-def run_evaluation(path, dataset_name):
+def run_evaluation(path:str):
+    """ Function to evaluate a single model.
+    Args:
+        path (str): model path
+    """
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("Device:{}".format(device))
-    
     # load model
     checkpoint = torch.load(path)
+
     model_name = checkpoint['model_name']
-    model = aux.get_model(model_name,checkpoint['num_classes'],model_type=FLAGS.modality)
+    modality = checkpoint['modality']
+    model = aux.get_model(model_name,checkpoint['num_classes'],model_type=modality)
     model.load_state_dict(checkpoint['model_state_dict'])
     model = model.to(device)
 
     # load data
+    dataset_name = checkpoint['dataset_name']
     mem_examples = checkpoint['mem_examples']
     train_examples = checkpoint['train_examples']
     load_dataset = getattr(datasets, 'get_'+dataset_name)
@@ -38,9 +39,9 @@ def run_evaluation(path, dataset_name):
     
     # perform validation
     loss_criterion = torch.nn.CrossEntropyLoss()
-    if FLAGS.modality == 'std':
+    if modality == 'std':
         best_acc, best_loss = aux.eval_std(model,test_loader,loss_criterion,device)
-        print("Best Loss:{:.4f} | Accuracy {:.2f} ".format(best_loss,best_acc))
+        print("Loss:{:.4f} | Accuracy {:.2f} ".format(best_loss,best_acc))
     else:
         cum_acc =  []
         for _ in range(10):
@@ -53,7 +54,7 @@ def run_evaluation(path, dataset_name):
 
 def main(argv=None):
 
-    run_evaluation(FLAGS.path_model,FLAGS.dataset)
+    run_evaluation(FLAGS.path_model)
 
 if __name__ == '__main__':
   absl.app.run(main)
