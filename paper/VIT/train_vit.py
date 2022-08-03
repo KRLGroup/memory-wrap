092868bd193ae604b4f113c743556c790a4eabbc
Biagio La Rosa <biagiomattialarosa@gmail.com>
2022-08-03 14:31:10 +0200

    removed unused code and aligned to PEP8

--- paper/VIT/train_vit.py
+++ paper/VIT/train_vit.py
@@ -1,17 +1,20 @@
 import argparse
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torchvision
 import pytorch_lightning as pl
 import warmup_scheduler
 import numpy as np
 from utils import get_model, get_dataset, get_experiment_name, get_criterion
 from da import CutMix, MixUp
+import numpy as np
+import random
+import os
+from typing import List
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--api-key", help="API Key for Comet.ml")
-parser.add_argument("--dataset", default="c10", type=str, help="[c10, c100, svhn]")
+parser.add_argument("--dataset", default="c10", type=str,
+                    help="[c10, c100, svhn]")
 parser.add_argument("--num-classes", default=10, type=int)
 parser.add_argument("--model-name", default="vit", help="[vit]", type=str)
 parser.add_argument("--patch", default=8, type=int)
@@ -48,45 +51,26 @@ parser.add_argument("--data_root", default="data", type=str)
 args = parser.parse_args()
 torch.manual_seed(args.seed)
 np.random.seed(args.seed)
-args.benchmark = True 
+args.benchmark = True
 args.gpus = torch.cuda.device_count()
 args.num_workers = 4*args.gpus if args.gpus else 8
 args.is_cls_token = True if not args.off_cls_token else False
 
 
-import numpy as np
-import random
-import os 
-from typing import List
-def split_dataset(dataset:torch.utils.data.Dataset,train_size:int,val_size:int,seed:int)->List[torch.utils.data.Dataset]:
-    """ Function to split a dataset
-    Args:
-        dataset (torch.utils.data.Dataset): dataset to be splitted
-        train_size (int): Number of samples to keep in training dataset
-        val_size (int): Number of samples to keep in validation dataset
-        seed (int): Seed to fix split for reproducibility
-    Returns:
-        List[torch.utils.data.Dataset]: The subsets of dataset chosen for
-            training dataset and validation dataset 
-    """
-    first_split = len(dataset) - val_size
-    data_rest, val_dataset = torch.utils.data.random_split(dataset,[first_split,val_size],generator=torch.Generator().manual_seed(seed))
-    size_train = min(len(data_rest),train_size)
-    train_dataset, _ = torch.utils.data.random_split(data_rest,[size_train,len(data_rest)-size_train], generator=torch.Generator().manual_seed(seed))
-    return train_dataset, val_dataset
 def set_seed(seed):
     """ Method to set seed across runs to ensure reproducibility.
     It fixes seed for single-gpu machines.
 
     Args:
         seed (int): Seed to fix reproducibility. It should different for
-            each run 
+            each run
 
     Returns:
         RandomState: fixed random state to initialize dataset iterators
     """
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False # set to false for reproducibility, True to boost performance
+    # set to false for reproducibility, True to boost performance
+    torch.backends.cudnn.benchmark = False
     torch.manual_seed(seed)
     np.random.seed(seed)
     torch.cuda.manual_seed(seed)
@@ -95,20 +79,36 @@ def set_seed(seed):
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
     return random_state
 
-def get_loaders_args(dataset_name, dataset_dir, batch_size_train, batch_size_test, train_examples, mem_examples, seed=42):
-    if dataset_name == 'c10':
-        name = 'CIFAR10'
-    elif dataset_name == 'svhn':
-        name = 'SVHN'
-    load_dataset = getattr(datasets, 'get_'+name)
-    loaders = load_dataset(dataset_dir,batch_size_train=batch_size_train, batch_size_test=batch_size_test,batch_size_memory=mem_examples,size_train=train_examples,seed=seed)
-    return loaders
+
+def split_dataset(dataset: torch.utils.data.Dataset, train_size: int,
+                  val_size: int, seed: int) -> List[torch.utils.data.Dataset]:
+    """ Function to split a dataset
+    Args:
+        dataset (torch.utils.data.Dataset): dataset to be splitted
+        train_size (int): Number of samples to keep in training dataset
+        val_size (int): Number of samples to keep in validation dataset
+        seed (int): Seed to fix split for reproducibility
+    Returns:
+        List[torch.utils.data.Dataset]: The subsets of dataset chosen for
+            training dataset and validation dataset
+    """
+    first_split = len(dataset) - val_size
+    data_rest, val_dataset = torch.utils.data.random_split(
+                        dataset, [first_split, val_size],
+                        generator=torch.Generator().manual_seed(seed))
+    size_train = min(len(data_rest), train_size)
+    train_dataset, _ = torch.utils.data.random_split(
+                        data_rest, [size_train, len(data_rest)-size_train],
+                        generator=torch.Generator().manual_seed(seed))
+    return train_dataset, val_dataset
+
 
 if not args.gpus:
-    args.precision=32
+    args.precision = 32
 
 if args.mlp_hidden != args.hidden*4:
-    print(f"[INFO] In original paper, mlp_hidden(CURRENT:{args.mlp_hidden}) is set to: {args.hidden*4}(={args.hidden}*4)")
+    print(f"[INFO] In original paper, mlp_hidden(CURRENT:{args.mlp_hidden}) "
+          f"is set to: {args.hidden*4}(={args.hidden}*4)")
 
 train_ds, test_ds = get_dataset(args)
 
@@ -130,23 +130,36 @@ class Net(pl.LightningModule):
         return self.model(x)
 
     def configure_optimizers(self):
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams.lr, betas=(self.hparams.beta1, self.hparams.beta2), weight_decay=self.hparams.weight_decay)
-        self.base_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=self.hparams.max_epochs, eta_min=self.hparams.min_lr)
-        self.scheduler = warmup_scheduler.GradualWarmupScheduler(self.optimizer, multiplier=1., total_epoch=self.hparams.warmup_epoch, after_scheduler=self.base_scheduler)
+        self.optimizer = torch.optim.Adam(
+            self.model.parameters(),
+            lr=self.hparams.lr,
+            betas=(self.hparams.beta1,
+                   self.hparams.beta2),
+            weight_decay=self.hparams.weight_decay)
+        self.base_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            self.optimizer,
+            T_max=self.hparams.max_epochs,
+            eta_min=self.hparams.min_lr)
+        self.scheduler = warmup_scheduler.GradualWarmupScheduler(
+            self.optimizer, multiplier=1.,
+            total_epoch=self.hparams.warmup_epoch,
+            after_scheduler=self.base_scheduler)
         return [self.optimizer], [self.scheduler]
 
     def training_step(self, batch, batch_idx):
         img, label = batch
         if self.hparams.cutmix or self.hparams.mixup:
             if self.hparams.cutmix:
-                img, label, rand_label, lambda_= self.cutmix((img, label))
+                img, label, rand_label, lambda_ = self.cutmix((img, label))
             elif self.hparams.mixup:
                 if np.random.rand() <= 0.8:
                     img, label, rand_label, lambda_ = self.mixup((img, label))
                 else:
-                    img, label, rand_label, lambda_ = img, label, torch.zeros_like(label), 1.
+                    img, label, rand_label, lambda_ = img, label, \
+                         torch.zeros_like(label), 1.
             out = self.model(img)
-            loss = self.criterion(out, label)*lambda_ + self.criterion(out, rand_label)*(1.-lambda_)
+            loss = self.criterion(out, label)*lambda_ + \
+                self.criterion(out, rand_label)*(1.-lambda_)
         else:
             out = self(img)
             loss = self.criterion(out, label)
@@ -172,10 +185,9 @@ class Net(pl.LightningModule):
         self.log("val_acc", acc)
         return loss
 
-
     def _log_image(self, image):
         grid = torchvision.utils.make_grid(image, nrow=4)
-        self.logger.experiment.log_image(grid.permute(1,2,0))
+        self.logger.experiment.log_image(grid.permute(1, 2, 0))
         print("[INFO] LOG IMAGE!!!")
 
 
@@ -198,28 +210,40 @@ if __name__ == "__main__":
             name=experiment_name
         )
         refresh_rate = 1
-    
+
     accuracies = []
-    for run in range(0,15):
+    for run in range(0, 15):
         set_seed(run)
         train_ds, test_ds = get_dataset(args)
-        train_data, _ = split_dataset(train_ds,args.data_samples,1,run)
+        train_data, _ = split_dataset(train_ds, args.data_samples, 1, run)
         set_seed(run)
-        
-        train_dl = torch.utils.data.DataLoader( train_data, batch_size=args.batch_size,pin_memory=True, shuffle=True, drop_last=True,worker_init_fn=run)
 
-        test_dl = torch.utils.data.DataLoader(test_ds, batch_size=args.eval_batch_size,pin_memory=True,worker_init_fn=run)
+        train_dl = torch.utils.data.DataLoader(
+            train_data, batch_size=args.batch_size, pin_memory=True,
+            shuffle=True, drop_last=True, worker_init_fn=run)
+
+        test_dl = torch.utils.data.DataLoader(
+            test_ds, batch_size=args.eval_batch_size,
+            pin_memory=True, worker_init_fn=run)
 
         net = Net(args)
-        trainer = pl.Trainer(precision=args.precision,fast_dev_run=args.dry_run, gpus=args.gpus, benchmark=args.benchmark, logger=logger, max_epochs=args.max_epochs, enable_model_summary=False, progress_bar_refresh_rate=refresh_rate)
+        trainer = pl.Trainer(precision=args.precision,
+                             fast_dev_run=args.dry_run, gpus=args.gpus,
+                             benchmark=args.benchmark, logger=logger,
+                             max_epochs=args.max_epochs,
+                             enable_model_summary=False,
+                             progress_bar_refresh_rate=refresh_rate)
 
-        trainer.fit(model=net, train_dataloaders=train_dl, val_dataloaders=test_dl)
+        trainer.fit(model=net, train_dataloaders=train_dl,
+                    val_dataloaders=test_dl)
         print(trainer.callback_metrics['val_acc'])
         accuracies.append(trainer.callback_metrics['val_acc'])
         if not args.dry_run:
             model_path = f"weights/{experiment_name}.pth"
             torch.save(net.state_dict(), model_path)
             if args.api_key:
-                logger.experiment.log_asset(file_name=experiment_name, file_data=model_path)
+                logger.experiment.log_asset(file_name=experiment_name,
+                                            file_data=model_path)
 
-        print("Run:{} | Mean Accuracy:{:.4f} | Std Dev Accuracy:{:.4f}\t".format(run+1,np.mean(accuracies), np.std(accuracies)))
+        print(f"Run:{run+1} | Mean Accuracy:{np.mean(accuracies):.4f} |"
+              f"Std Dev Accuracy:{np.std(accuracies):.4f}\t")
